4c41fe344d1cf624c863d40cb9f5ef9ece7b0e90
hyeon <nguswjd02@gmail.com>
2025-11-17 00:23:31 +0900

    로컬 프론트 개발 중에도 /signup API 호출 가능

--- src/main/java/com/pet/petCare/WebConfig.java
+++ src/main/java/com/pet/petCare/WebConfig.java
@@ -9,7 +9,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://pet-care-2025.vercel.app")
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "https://pet-care-2025.vercel.app"
+                )
                 .allowedMethods("GET","POST","PUT","DELETE","OPTIONS");
     }
 }
